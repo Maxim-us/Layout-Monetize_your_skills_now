a2d21823b163c1e27848e7d50fe5f3c3392d4eec
Maksym Marko <maximus21@mail.ru>
2016-03-07 08:58:15 +0200

    Start of work on layout

---
+++ js/common.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	
+	//owl
+	$(".owl-carousel").owlCarousel({  	
+		loop : true,
+		autoplay : true,
+		autoplayTimeout : 5000,
+		items : 1,
+		smartSpeed : 1000,
+		center : true,
+		autoplaySpeed : 2000,
+		mouseDrag : false,
+		nav : true,
+		navText : '',
+		dots : true
+	});
+	
+	/*
+	if(navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1){					
+		$(".test-our_services_all").hide();
+		$(".test-our_services_safari").show();
+		$(".test-about_all").hide();
+		$(".test-about_safari").show();			
+	}
+	*/
+})
